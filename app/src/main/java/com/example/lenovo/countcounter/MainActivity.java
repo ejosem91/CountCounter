007ef3cbf162d1d11962748f22c93e3e2ea0fafd
green91 <e.josem91@gmail.com>
2016-10-04 06:19:42 -0600

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/countcounter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.countcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
